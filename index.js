a3db88cf7c3925620397163c9ae893cc6b130af0
Adrian Jenkins <adrianjnkns@gmail.com>
2021-09-18 20:45:38 -0600

    Having trouble with null

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 (async () => {
+    try{
 
     const USERNAME = 'leomessi';
     const BASE_URL = `https://instagram.com/${ USERNAME }`;
@@ -14,7 +15,8 @@ const cheerio = require('cheerio');
 
     let script_regex = /window._sharedData = (.+);/g.exec(script);
 
-    let { entry_data: { ProfilePage : {[0] : { graphql: { user } }  } } } = JSON.parse(script_regex[1]);
+
+    let { entry_data: { ProfilePage : { [0] : { graphql: { user } } } } } = JSON.parse(script_regex[1]);
 
     let ig_data = {
         fullName: user.full_name,
@@ -24,6 +26,24 @@ const cheerio = require('cheerio');
         isBusinessAccount: user.is_business_account,
         postsQty: user.edge_owner_to_timeline_media.count
     };
-    console.log(ig_data);
+
+    let { entry_data: { ProfilePage : {[0] : { graphql: { user : { edge_owner_to_timeline_media } } }  } } } = JSON.parse(script_regex[1]);
+
+    const posts = [];
+    for(elem of edge_owner_to_timeline_media.edges){
+     posts.push({  
+        id: elem.node.id,
+        caption: elem.node.text,
+        is_video: elem.node.is_video,
+        accessCaption: elem.node.accessibility_caption,
+        commentQty: elem.node.edge_media_to_comment.count,
+        timestamp: elem.node.taken_at_timestamp,
+        liksQty: elem.node.edge_liked_by.count
+    })
+}
+    console.log(posts);
+    } catch(err){
+     return   console.log(err);
+    }
 })()
 
