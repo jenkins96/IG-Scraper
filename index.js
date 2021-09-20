8b38efbaa6af457b0d93af326943039ce8540e7a
Adrian Jenkins <adrianjnkns@gmail.com>
2021-09-19 20:40:10 -0600

    Done

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
+const request = require("request-promise");
+const cheerio = require("cheerio");
 
 (async () => {
-    try{
-
-    const USERNAME = 'leomessi';
-    const BASE_URL = `https://instagram.com/${ USERNAME }`;
+  try {
+    const USERNAME = "leomessi";
+    const BASE_URL = `https://instagram.com/${USERNAME}`;
 
     let response = await request(BASE_URL);
 
@@ -15,35 +14,52 @@ const cheerio = require('cheerio');
 
     let script_regex = /window._sharedData = (.+);/g.exec(script);
 
+    let {
+      entry_data: {
+        ProfilePage: {
+          [0]: {
+            graphql: { user },
+          },
+        },
+      },
+    } = JSON.parse(script_regex[1]);
 
-    let { entry_data: { ProfilePage : { [0] : { graphql: { user } } } } } = JSON.parse(script_regex[1]);
-
-    let ig_data = {
-        fullName: user.full_name,
-        username: user.username,
-        biography: user.biography,
-        followersQty: user.edge_followed_by.count,
-        isBusinessAccount: user.is_business_account,
-        postsQty: user.edge_owner_to_timeline_media.count
-    };
-
-    let { entry_data: { ProfilePage : {[0] : { graphql: { user : { edge_owner_to_timeline_media } } }  } } } = JSON.parse(script_regex[1]);
+    let {
+      entry_data: {
+        ProfilePage: {
+          [0]: {
+            graphql: {
+              user: { edge_owner_to_timeline_media },
+            },
+          },
+        },
+      },
+    } = JSON.parse(script_regex[1]);
 
     const posts = [];
-    for(elem of edge_owner_to_timeline_media.edges){
-     posts.push({  
+    for (elem of edge_owner_to_timeline_media.edges) {
+      posts.push({
         id: elem.node.id,
         caption: elem.node.text,
         is_video: elem.node.is_video,
         accessCaption: elem.node.accessibility_caption,
         commentQty: elem.node.edge_media_to_comment.count,
         timestamp: elem.node.taken_at_timestamp,
-        liksQty: elem.node.edge_liked_by.count
-    })
-}
-    console.log(posts);
-    } catch(err){
-     return   console.log(err);
+        liksQty: elem.node.edge_liked_by.count,
+      });
     }
-})()
 
+    let ig_data = {
+      fullName: user.full_name,
+      username: user.username,
+      biography: user.biography,
+      followersQty: user.edge_followed_by.count,
+      isBusinessAccount: user.is_business_account,
+      postsQty: user.edge_owner_to_timeline_media.count,
+      posts,
+    };
+    console.log(ig_data);
+  } catch (err) {
+    return console.log(err);
+  }
+})();
